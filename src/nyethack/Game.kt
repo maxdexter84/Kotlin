f31427557f796c050189aa6d3606553fea2585cb
maxdexter <maxdexter@bk.ru>
2020-06-18 17:02:35 +0300

    условное выражение вместо ветвления

--- src/nyethack/Game.kt
+++ src/nyethack/Game.kt
@@ -13,19 +13,20 @@ val auraVisible = isBlessed && healthPonts > 50 || isImmortal
     }else{
         println("NONE")
     }
-    if (healthPonts == 100){
-        println(name + " is in excellent condition!")
-    }else if(healthPonts >= 90){
-        println(name + " has a few scratches.")
+    val healthStatus = if (healthPonts == 100){
+        " is in excellent condition!"
+    } else if(healthPonts >= 90){
+        " has a few scratches."
     }else if (healthPonts >=75){
         if(isBlessed){
-            println(name + " has some minor wounds but is healing quite quickly!")
-        }else println(name + " has some minore wounds.")
+            " has some minor wounds but is healing quite quickly!"
+        }else " has some minore wounds."
     }else if (healthPonts >= 15){
-        println(name + " looks pretty hurt.")
+        " looks pretty hurt."
     }else{
-        println(name + " is in awful condition!.")
+        " is in awful condition!."
     }
 
-
+//Состояние игрока
+    println(name + healthStatus)
 }
