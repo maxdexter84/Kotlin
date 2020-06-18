cff78f31533e81e6f412b000488a0c19e207668a
maxdexter <maxdexter@bk.ru>
2020-06-18 22:22:40 +0300

    заменил в условных выражениях сравнение на интервалы( вместо >= 75 , интервал in 75..89)

--- src/nyethack/Game.kt
+++ src/nyethack/Game.kt
@@ -1,27 +1,24 @@
 package nyethack
 
 fun main(){
-val name = "Madrigal"
+    val name = "Madrigal"
 var healthPonts = 89
 val isBlessed = true
 val isImmortal = false
 val auraVisible = isBlessed && healthPonts > 50 || isImmortal
 
 //Аура
-    if(auraVisible){
-        println("GREEN")
-    }else{
-        println("NONE")
-    }
+    val  auraColor = if (auraVisible)"GREEN" else "NONE"
+    println(auraColor)
     val healthStatus = if (healthPonts == 100){
         " is in excellent condition!"
-    } else if(healthPonts >= 90){
+    } else if(healthPonts in 90..99){
         " has a few scratches."
-    }else if (healthPonts >=75){
+    }else if (healthPonts in 75..89){
         if(isBlessed){
             " has some minor wounds but is healing quite quickly!"
         }else " has some minore wounds."
-    }else if (healthPonts >= 15){
+    }else if (healthPonts in 15..74){
         " looks pretty hurt."
     }else{
         " is in awful condition!."
